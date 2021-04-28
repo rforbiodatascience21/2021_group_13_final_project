f375af7ac9cef4fd6c4fd3c9645d329f96db1a95
SofusHW <sofus.wii@gmail.com>
2021-04-28 20:55:21 +0200

    First patient load, succesfull tibble

--- R/01_load.R
+++ R/01_load.R
@@ -11,12 +11,19 @@ library(Seurat)
 source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-GSM4058963_025I <- readRDS(file = "Data/_raw/GSM4058963_025I.dgecounts.rds")
+GSM4058963_025I <- read_rds(file = "Data/_raw/GSM4058963_025I.dgecounts.rds")
 
 
 # Wrangle data ------------------------------------------------------------
-dataframe_wrangle <- dataframe %>% operations
+# We only want to look at the exons.
+umicount_exon_reads <- GSM4058963_025I %>% 
+  pluck("umicount") %>% 
+  pluck("exon") %>% 
+  pluck("all")
 
+view(umicount_exon_reads)
+umicount_exon_reads %>% 
+  select("AAACCTGAGCAGCCTC")
 
 # Write data --------------------------------------------------------------
 write_tsv(dataframe_wrangle,
