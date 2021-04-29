c5b2db4d11aa7b123bede90a4c9d2fec9491e399
Karl <karl.kaether@posteo.de>
2021-04-29 21:06:46 +0200

    wrote refining functions

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -1,9 +1,12 @@
 # Define project functions ------------------------------------------------
+
+### loading functions ###-------------------------------------------------------
 make_list_of_patient_data <- function(list_which_patients){
   data <- list()
   for (patient in list_which_patients){
     data <- c(data,list(read_rds(file = paste0("Data/_raw/",patient,".dgecounts.rds"))))
   }
+  
   return(data)
 }
 
@@ -24,6 +27,8 @@ write_umicounts <- function(patient_data,patient_name){
             path = paste0("Data/01_data_",patient_name,".csv.gz"))
 }
 
+### cleaning functions ###-------------------------------------------------------
+
 load_umicounts <- function(patient_name){
   read_csv(paste0("Data/01_data_",patient_name,".csv.gz"))
 }
@@ -33,6 +38,12 @@ remove_zero_rows <-function(patient){
     patient %>% 
     mutate(row_sum=
              rowSums(patient))
+    # this is apparently tidyverse but doesnt work
+    #https://dplyr.tidyverse.org/articles/rowwise.html
+    #rowwise()
+    #mutate(row_sum=
+    #         sum(c_across())
+    #)
   patient <-
     patient %>% 
     filter(row_sum != 0)
@@ -45,5 +56,27 @@ get_range <- function(patient){
     patient %>% 
     select(row_sum) %>% 
     arrange(row_sum)
+  
   return(range)
+}
+
+refine_range <- function(range, lower_cutoff){
+  nineteeth <- 
+    range %>% 
+    pull(1) %>% 
+    quantile(0.9)
+  range <-
+    range %>% 
+    filter(row_sum > lower_cutoff & row_sum < nineteeth)
+  
+  return(range)
+}
+
+refine_data <- function(data,ranges_refined){
+  data <-
+    inner_join(
+      data,ranges_refined,
+    )
+  
+  return(data)
 }
