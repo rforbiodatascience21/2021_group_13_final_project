724f95f3085e737b39fcc9ca42e1cc052bd6c171
Karl <karl.kaether@posteo.de>
2021-04-29 12:16:12 +0200

    wrote function for loading and saving data and removing zero rows. sorry sofus i deleted your function

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -1,17 +1,42 @@
 # Define project functions ------------------------------------------------
-foo <- function(x){
-  return(2*x)
-}
-bar <- function(x){
-  return(x^2)
+make_list_of_patient_data <- function(list_which_patients){
+  data <- list()
+  for (patient in list_which_patients){
+    data <- c(data,list(read_rds(file = paste0("Data/_raw/",patient,".dgecounts.rds"))))
+  }
+  return(data)
 }
 
-# Function for loading patients
-load_patients <-function(patientfile){
-  patientfile[21:25] <- read_rds(file = "Data/_raw/"+patientfile)
-  umicount_exon_ <- patientfile[21:25] %>% 
-    pluck("umicount") %>% 
-    pluck("exon") %>% 
-    pluck("all") %>% 
-    as_tibble()
+get_exon_umicounts <-function(patient){
+ 
+  umicount_exon_reads <- 
+      as.tibble(patient) %>% 
+      pluck("umicount") %>% 
+      pluck("exon") %>% 
+      pluck("all") %>% 
+      as_tibble()
+  
+  return(umicount_exon_reads)
+}
+
+write_umicounts <- function(patient_data,patient_name){
+  write_csv(patient_data,
+            path = paste0("Data/01_data_",patient_name,".csv.gz"))
+}
+
+load_umicounts <- function(patient_name){
+  read_csv(paste0("Data/01_data_",patient_name,".csv.gz"))
+}
+
+remove_zero_rows <-function(patient){
+  patient <-
+    patient %>% 
+    mutate(row_sum=
+             rowSums(patient),
+                           .before=AAACCTGAGCAGCCTC)
+  patient <-
+    patient %>% 
+    filter(total==0)
+  
+  return(patient)
 }
