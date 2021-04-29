9dd3370346d2a9295bed33a81cf8d375207ed6a9
Karl <karl.kaether@posteo.de>
2021-04-29 16:54:13 +0200

    started EDA to see ranges of umi counts with the aim of sensibly removing low and high ones

--- R/02_clean.R
+++ R/02_clean.R
@@ -5,6 +5,7 @@
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library(patchwork)
 
 
 # Define functions --------------------------------------------------------
@@ -17,8 +18,38 @@ patients <-
 
 data <- map(patients,load_umicounts)
 
-# Wrangle data ------------------------------------------------------------
+### Wrangle data ------------------------------------------------------------
+
+# check if there are rows with only zeros == cells with no expression, would correspond to empty droplets
 data <- map(data,remove_zero_rows)
+
+# no there arent. now check if there are any with particularly high umi count which could correspond to droplets with more than one cell
+
+# get all the counts
+ranges <- map(data,get_range)
+
+# visualize to help understand what could be filtered out
+
+patient1 <-
+  ggplot(
+    pluck(ranges,1),
+    aes(row_sum)) + 
+  geom_histogram()
+
+# there is a big range
+
+hist_data1 <-
+  ranges %>% 
+  pluck(1) %>% 
+  filter(row_sum>10000)
+
+# the following graph needs improvement
+
+patient1 <-
+  ggplot(hist_data1,aes(row_sum)) + 
+  geom_freqpoly()
+
+# lets next find good values to cut off (the small and the high ones)
 
 # Write data --------------------------------------------------------------
 
