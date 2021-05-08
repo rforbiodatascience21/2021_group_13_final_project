3ee9b9778ecba606e8146fe0661d21e56a491193
Karl <karl.kaether@posteo.de>
2021-05-08 19:36:10 +0200

    second plot

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -32,10 +32,7 @@ cells_per_group_and_patient <-
   group_by(group,Patient_ID) %>% 
   summarise(total=n())
 
-
-#levels=c("003C","465C","065CO","207CO","025I","177I")
-
-#cells_plot <- 
+#cells_count_plot <- 
   cells_per_group_and_patient %>%
   ggplot(aes(x=group,y=total)) +
   geom_col(aes(fill = fct_reorder2(as.factor(Patient_ID),total,group))) +
@@ -45,6 +42,47 @@ cells_per_group_and_patient <-
   labs(
     title = "Cell Counts per Group and Patient after Filtering"
   )
+
+###How many cells are there of each type in each group 
+  
+sum_per_group <-
+    data %>% 
+    group_by(group) %>% 
+    summarise(total_count=n())
+  
+cells_type_per_group <-
+  data %>% 
+  group_by(group,CellType_Category) %>% 
+  summarise(cell_count=n()) %>% 
+  pivot_wider(
+    names_from = CellType_Category,
+    values_from = cell_count
+  ) %>% 
+  ungroup() %>% 
+  left_join(
+    sum_per_group
+  ) %>% 
+  pivot_longer(
+    Endothelial:Stromal,
+    names_to = "CellType_Category",
+    values_to = "cell_count"
+  )
+
+# cells_type_plot
+  cells_type_per_group %>% 
+    ggplot(aes(x=group,y=cell_count/total_count)) +
+    geom_col(aes(fill = fct_reorder2(as.factor(CellType_Category),cell_count,group))) +
+    scale_fill_viridis_d("Type",alpha=0.6) +
+    theme_minimal() +
+    coord_flip() +
+    ylab("relative count") +
+    labs(title = "Distribution of Cell Types per Group") +
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
+  
+#cells_type_plot
+  
 
 # Plot Data ---------------------------------------------------------------
 
