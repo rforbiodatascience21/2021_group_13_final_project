640b47f9475ada7c41e6730dff60293cf5bf7358
SofusHW <sofus.wii@gmail.com>
2021-05-10 20:45:28 +0200

    Clean up, leonify.

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,103 +1,94 @@
-# Clear workspace ---------------------------------------------------------
+# Clear workspace --------------------------------------------------------------
 #rm(list = ls())
 
-# Load libraries ----------------------------------------------------------
+# Load libraries ---------------------------------------------------------------
 library(tidyverse)
 
-# Patchwork if we want to plot the boxplots below of more than one patient together
+# Patchwork if we want to plot the boxplots below of more than one patient 
+# together
 library(patchwork)
 
-# Define functions --------------------------------------------------------
+# Define functions -------------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
-# Load data ---------------------------------------------------------------
-
+# Load data --------------------------------------------------------------------
 # Patient 1
-patient_1 <- 
-  read_csv(file = "Data/01_patient_1I.csv.gz")
+patient_1 <- read_csv(file = "Data/01_patient_1I.csv.gz")
 
 
 # Patient_2
-patient_2 <- 
-  read_csv(file = "Data/01_patient_2C.csv.gz")
+patient_2 <- read_csv(file = "Data/01_patient_2C.csv.gz")
 
 
 # Patient_3
-patient_3 <- 
-  read_csv(file = "Data/01_patient_3C.csv.gz")
+patient_3 <- read_csv(file = "Data/01_patient_3C.csv.gz")
 
 
 # Patient_4
-patient_4 <- 
-  read_csv(file = "Data/01_patient_4CO.csv.gz")
+patient_4 <- read_csv(file = "Data/01_patient_4CO.csv.gz")
 
 
 # Patient_5
-patient_5 <- 
-  read_csv(file = "Data/01_patient_5CO.csv.gz")
+patient_5 <- read_csv(file = "Data/01_patient_5CO.csv.gz")
 
 
 # Patient_6
-patient_6 <-
-  read_csv(file = "Data/01_patient_6I.csv.gz")
+patient_6 <- read_csv(file = "Data/01_patient_6I.csv.gz")
 
 
 # Combining patients -----------------------------------------------------------
 
-### Wrangle data ---------------------------------------------------------------
+# Wrangle data -----------------------------------------------------------------
 
-# ------------------- getting gene names and cell bar codes --------------------
+# ------------------- Getting gene names and cell bar codes --------------------
 
 # Loading metadata and getting cell types --------------------------------------
 # Metadata supplied by the authors of the paper("ids.csv")
 # This is just done for pretty gene names and does not provide any other benefit
-gene_mapping <- 
-  read_csv("Data/_raw/ids.csv",
-           col_names=c("value","Gene")) %>%
+gene_mapping <- read_csv("Data/_raw/ids.csv",
+                         col_names=c("value","Gene")) %>%
   tibble()
-  
-  
-# Patient_1
-# transpose so that genes are columns and rows are cells
-patient_1 <-
-  patient_1 %>% 
-  #select(-X1) %>% 
+
+
+# Patient_1                ---------------------------
+# Transpose so that genes are columns and rows are cells
+patient_1 <- patient_1 %>% 
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname,
               values_from = value)
 
 Genes <- read_csv("Data/Genes_1")
-Genes <-
-  Genes %>% 
-  left_join(gene_mapping) %>% 
+Genes <- Genes %>%
+  left_join(gene_mapping) %>%
   pluck("Gene")
 
-# rename genes and make nice name for column with barcodes
-colnames(patient_1) <- 
-  c("Cell_Barcode",Genes)
+
+# Rename genes and make nice name for column with barcodes
+colnames(patient_1) <- c("Cell_Barcode",
+                         Genes)
 
 
 # -------------- now do the same for 5 other patients --------------
 
 #Patient_2                ---------------------------
-patient_2 <-
-patient_2 %>%
+patient_2 <- patient_2 %>%
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname,
               values_from = value)
 
 Genes <- read_csv("Data/Genes_2")
-Genes <-Genes %>% 
+Genes <- Genes %>% 
   left_join(gene_mapping) %>% 
   pluck("Gene")
 
-colnames(patient_2) <- c("Cell_Barcode",Genes)
+colnames(patient_2) <- c("Cell_Barcode",
+                         Genes)
 
 
 #Patient_3                ---------------------------
-patient_3 <-patient_3 %>% 
+patient_3 <- patient_3 %>% 
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname,
@@ -108,61 +99,59 @@ Genes <-Genes %>%
   left_join(gene_mapping) %>% 
   pluck("Gene")
 
-colnames(patient_3) <- c("Cell_Barcode",Genes)
+colnames(patient_3) <- c("Cell_Barcode",
+                         Genes)
 
 
 #Patient_4                ---------------------------
-patient_4 <-
-  patient_4 %>% 
+patient_4 <- patient_4 %>% 
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname,
               values_from = value)
 
 Genes <- read_csv("Data/Genes_4")
-Genes <-
-  Genes %>% 
+Genes <- Genes %>% 
   left_join(gene_mapping) %>% 
   pluck("Gene")
 
-colnames(patient_4) <- c("Cell_Barcode",Genes)
+colnames(patient_4) <- c("Cell_Barcode",
+                         Genes)
 
 
 #Patient_5                ---------------------------
-patient_5 <-
-  patient_5 %>% 
+patient_5 <- patient_5 %>% 
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname,
               values_from = value)
 
 Genes <- read_csv("Data/Genes_5")
-Genes <-
-  Genes %>% 
+Genes <- Genes %>% 
   left_join(gene_mapping) %>% 
   pluck("Gene")
 
-colnames(patient_5) <- c("Cell_Barcode",Genes)
+colnames(patient_5) <- c("Cell_Barcode",
+                         Genes)
 
 
 #Patient_6                ---------------------------
-patient_6 <-
-  patient_6 %>% 
+patient_6 <- patient_6 %>% 
   rownames_to_column() %>%  
   pivot_longer(-rowname) %>% 
   pivot_wider(names_from = rowname, 
               values_from = value)
 
 Genes <- read_csv("Data/Genes_6")
-Genes <-
-  Genes %>% 
+Genes <- Genes %>% 
   left_join(gene_mapping) %>% 
   pluck("Gene")
 
-colnames(patient_6) <- c("Cell_Barcode",Genes)
+colnames(patient_6) <- c("Cell_Barcode",
+                         Genes)
+
 
 # Write data -------------------------------------------------------------------
-
 # Patient_1
 write.csv(patient_1,
           file = "Data/02_patient_1I.csv.gz")
@@ -188,4 +177,11 @@ write.csv(patient_6,
           file = "Data/02_patient_6I.csv.gz")
 
 # Remove Data ------------------------------------------------------------------
-rm(patient_1, patient_2, patient_3, patient_4, patient_5, patient_6, Genes, gene_mapping)
+rm(patient_1,
+   patient_2,
+   patient_3,
+   patient_4,
+   patient_5,
+   patient_6,
+   Genes,
+   gene_mapping)
--- R/03_augment.R
+++ R/03_augment.R
@@ -4,73 +4,59 @@
 # Load libraries ---------------------------------------------------------------
 library("tidyverse")
 
-
 # Define functions -------------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
-
 # Load data --------------------------------------------------------------------
 # Patient_1
-patient_1 <- 
-  read_csv(file = "Data/02_patient_1I.csv.gz")
+patient_1 <- read_csv(file = "Data/02_patient_1I.csv.gz")
 
-patient_1 <-
-  patient_1 %>% 
+patient_1 <- patient_1 %>% 
   mutate(Patient_ID = "025I",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
   slice(-1)
 
 # Patient_2                ---------------------------
-patient_2 <- 
-  read_csv(file = "Data/02_patient_2C.csv.gz")
+patient_2 <- read_csv(file = "Data/02_patient_2C.csv.gz")
 
-patient_2 <-
-  patient_2 %>% 
+patient_2 <- patient_2 %>% 
   mutate(Patient_ID = "465C",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
   slice(-1)
 
 # Patient_3                ---------------------------
-patient_3 <- 
-  read_csv(file = "Data/02_patient_3C.csv.gz")
+patient_3 <- read_csv(file = "Data/02_patient_3C.csv.gz")
 
-patient_3 <-
-  patient_3 %>% 
+patient_3 <- patient_3 %>% 
   mutate(Patient_ID = "003C",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
   slice(-1)
 
 # Patient_4                ---------------------------
-patient_4 <- 
-  read_csv(file = "Data/02_patient_4CO.csv.gz")
+patient_4 <- read_csv(file = "Data/02_patient_4CO.csv.gz")
 
-patient_4 <-
-  patient_4 %>% 
+patient_4 <- patient_4 %>% 
   mutate(Patient_ID = "207CO",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
   slice(-1)
 
 # Patient_5                ---------------------------
-patient_5 <- 
-  read_csv(file = "Data/02_patient_5CO.csv.gz")
+patient_5 <- read_csv(file = "Data/02_patient_5CO.csv.gz")
 
-patient_5 <-
-  patient_5 %>% 
+patient_5 <- patient_5 %>% 
   mutate(Patient_ID = "056CO",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
   slice(-1)
 
 # Patient_6                ---------------------------
-patient_6 <- 
-  read_csv(file = "Data/02_patient_6I.csv.gz")
+patient_6 <- read_csv(file = "Data/02_patient_6I.csv.gz")
 
-patient_6 <-
-  patient_6 %>% 
+patient_6 <- patient_6 %>% 
   mutate(Patient_ID = "177I",
          .before = Cell_Barcode) %>% 
   select(-1) %>% 
@@ -88,20 +74,25 @@ data <- bind_rows(
 
 
 # Replacing NAs (gene not present in the patient but in another) by 0
-data <-
-  data %>%
+data <- data %>%
   mutate(across(everything(),
-           ~replace_na(.x,0))) %>% 
+           ~replace_na(.x,
+                       0))) %>% 
   select(-starts_with("ENSG"))
 
 
 # Clearing environment
-rm(patient_1, patient_2, patient_3, patient_4, patient_5, patient_6)
+rm(patient_1, 
+   patient_2,
+   patient_3,
+   patient_4,
+   patient_5,
+   patient_6)
 
 
 # Loading metadata -------------------------------------------------------------
-metadata <-
-  read_tsv("Data/_raw/GSE136831_AllCells.Samples.CellType.MetadataTable.txt") %>% 
+metadata <- read_tsv(
+  "Data/_raw/GSE136831_AllCells.Samples.CellType.MetadataTable.txt") %>% 
   tibble() %>% 
   select(CellBarcode_Identity,
          nUMI,
@@ -149,58 +140,52 @@ data <-
 rm(metadata)
 
 
-# Now there are quite some NAs as the metadata apparently contains only cells filtered by the authors
-# we will filter further to second-check this filtering. thus if it works well, the NAs will mostly be gone
+# Now there are quite some NAs as the metadata apparently contains only cells 
+# filtered by the authors.
+# We will filter further to second-check this filtering. Thus if it works well, 
+# the NAs will mostly be gone
 
 # Wrangle data -----------------------------------------------------------------
 
-# Combine the patients' gene expression to the metadata data of the cells
-# run patient_slicer, meta_slicer, combiner and binder
+# Combine the patients' gene expression to the metadata data of the cells.
+# Run patient_slicer, meta_slicer, combiner and binder
 
-# If there are cells with fewer than 2000 transcripts recorded, these cells are filtered out from the 10000 starting cell count per patient
-data <-
-  data %>% 
+# If there are cells with fewer than 2000 transcripts recorded, these cells are
+# filtered out from the 10000 starting cell count per patient
+data <- data %>% 
   filter(nUMI > 2000)
 
 
-# Filtering out the cells where the transcripts of the mitochondrial genes represent more 20% of the sum of the transcripts for a cell
+# Filtering out the cells where the transcripts of the mitochondrial genes 
+# represent more than 20% of the sum of the transcripts for a cell
 # Run mito_filter
 mt_selection <- select(data, 
                        starts_with("MT"))
 
 mt_sum <- mt_selection %>% 
-  mutate(
-    mito_sum =
-      rowSums(mt_selection))
+  mutate(mito_sum = rowSums(mt_selection))
 
 data <- data %>%
-  mutate(
-    select(
-      mt_sum,
-      mito_sum),
-    .after = Cell_Barcode) %>%
-  filter(
-    mito_sum /
-      nUMI < 0.2)
+  mutate(select(mt_sum,
+                mito_sum),
+         .after = Cell_Barcode) %>%
+  filter(mito_sum / nUMI < 0.2)
 
 
-# now check if there are any NAs remaining
+# Now check if there are any NAs remaining
 data %>% 
   filter(
-  across(
-    .cols = everything(),
-    .fns = ~ is.na(.x)
-  )
-)
-
+  across(.cols = everything(),
+         .fns = ~ is.na(.x)))
 
 # --> no NAs left
+
 
 # Write data -------------------------------------------------------------------
 write_csv(data, 
           file = "Data/03_data.csv")
 
-
 # Remove Data ------------------------------------------------------------------
-rm(data,mt_selection,mt_sum)
-
+rm(data,
+   mt_selection,
+   mt_sum)
