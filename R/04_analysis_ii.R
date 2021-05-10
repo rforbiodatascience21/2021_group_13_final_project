c4bae929c57ad8de9d70b51cc40abb02a7ddbf36
SofusHW <sofus.wii@gmail.com>
2021-05-10 22:04:25 +0200

    leonify

--- R/04_analysis_ii.R
+++ R/04_analysis_ii.R
@@ -94,34 +94,38 @@ IPF_model <- ciliated_IPF %>%
 
 
 # Clustering, dendrograms and heatmaps -----------------------------------------
-
 dendro <- COPD_model %>%
-  select(gene,p.value) %>%
+  select(gene,
+         p.value) %>%
   filter(!is.na(p.value))
 
 dendro <- column_to_rownames(dendro,
-                           var = "gene")
+                             var = "gene")
 
+hc <- hclust(dist(dendro),
+             "ave")
 
-hc<- hclust(dist(dendro), "ave")
-
-hcdata <- dendro_data(hc, type = "rectangle")
+hcdata <- dendro_data(hc,
+                      type = "rectangle")
 ggplot() +
   geom_segment(data = segment(hcdata), 
-               aes(x = x, y = y, xend = xend, yend = yend)
-  ) +
+               aes(x = x,
+                   y = y,
+                   xend = xend,
+                   yend = yend)) +
   geom_text(data = label(hcdata), 
-            aes(x = x, y = y, label = label, hjust = 0), 
-            size = 3
-  ) +
+            aes(x = x,
+                y = y,
+                label = label,
+                hjust = 0), 
+            size = 3) +
   coord_flip() +
-  scale_y_reverse(expand = c(0.2, 0))
+  scale_y_reverse(expand = c(0.2,
+                             0))
 
 ggdendrogram(hc)
 
-
 ggdendrogram(hc, rotate = TRUE)
-
 
 
 #A non-tidyverse compatible clustering method - NOT USED
