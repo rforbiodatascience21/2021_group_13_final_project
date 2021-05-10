9086f0c8d55b373e80b7822673e2b9a3b6a1f16d
ThanosPAS <thanospas@yahoo.com>
2021-05-10 18:18:11 +0200

    Clustering of genes and dendograms of clusters

--- R/04_analysis_ii.R
+++ R/04_analysis_ii.R
@@ -12,7 +12,7 @@ source(file = "R/99_project_functions.R")
 
 # Load data --------------------------------------------------------------------
 data <-
-  read_csv("Data/03_data1.csv")
+  read_csv("Data/03_data.csv")
 
 
 gois <-
@@ -30,14 +30,13 @@ ciliated <-data %>%
 
 ciliated_gois<-right_join(ciliated, gois, by=c("gene")) 
 
-
-
-
 ciliated_COPD<- ciliated_gois%>% 
   select(group,
          gene,
          Counts) %>%
-  mutate(group = case_when(group == "Control" ~ 0,
+  filter(!is.na(Counts)) %>% 
+  mutate(Counts = normalise(Counts),
+         group = case_when(group == "Control" ~ 0,
                            group == "COPD" ~ 1)) %>% 
   filter(group != "IPF") %>% 
   group_by(gene) %>% 
@@ -67,14 +66,32 @@ COPD_model <-
 
 
 
+dendro <- COPD_model%>%
+  select(gene,p.value)%>%
+  filter(!is.na(p.value))
+dendro<-column_to_rownames(dendro, var = "gene")
+
+d<-dist(sqrt(dendro))       
+ 
+dend_row <- d %>% hclust(method = "average") %>% as.dendrogram 
+dend_row %>% highlight_branches %>% plot
+
+
+dend_k <- find_k(dend_row)
+
+plot(dend_k)
+
+heatmaply(sqrt(dendro), Colv = NULL, hclust_method = "average", 
+          fontsize_row = 8,fontsize_col = 6,
+          k_row = NA, margins = c(60,170, 70,40),
+          xlab = "p value", ylab = "genes", main = "A smoking good plot on KOL"
+) 
 
 
 
-
-
-
-
-
-
-         
-         
+heatmaply(sqrt(dendro), Colv = NULL, hclust_method = "average", 
+          fontsize_row = 8,fontsize_col = 6,
+          k_row = NA, margins = c(60,50, 70,90),
+          xlab = "p value", ylab = "genes", main = "A smoking good plot on KOL",
+          plot_method = "plotly", row_dend_left = TRUE
+) 
