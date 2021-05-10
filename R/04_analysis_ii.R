27e09ffa4c240d46477c7e84893768a5319032ae
ThanosPAS <thanospas@yahoo.com>
2021-05-10 19:38:37 +0200

    Tidying and commenting

--- R/04_analysis_ii.R
+++ R/04_analysis_ii.R
@@ -45,7 +45,7 @@ ciliated_COPD<- ciliated_gois%>%
   nest() %>% 
   ungroup()
 
-
+#Modelling the COPD vs Control
 COPD_model <-
   ciliated_COPD %>% 
   mutate(mdl = map(data,
@@ -76,29 +76,22 @@ dendro<-column_to_rownames(dendro, var = "gene")
 d<-dist(sqrt(dendro))       
  
 dend_row <- d %>% hclust(method = "average") %>% as.dendrogram 
-dend_row %>% highlight_branches %>% plot
+dendro_plot<-dend_row %>% highlight_branches %>% plot
+
 
 
 dend_k <- find_k(dend_row)
 
-plot(dend_k)
+n_clusters<- plot(dend_k)
 
 
-heatmaply(sqrt(dendro), Colv = NULL, hclust_method = "average", 
-          fontsize_row = 8,fontsize_col = 6,
-          k_row = NA, margins = c(60,170, 70,40),
-          xlab = "p value", ylab = "genes", main = "A smoking good plot on KOL"
-) 
-
-
-
-heatmaply(sqrt(dendro), Colv = NULL, hclust_method = "average", 
+#Plotting the relationship of genes by significance
+heat_dendro<-heatmaply(sqrt(dendro), Colv = NULL, hclust_method = "average", 
           fontsize_row = 8,fontsize_col = 6,
           k_row = NA, margins = c(60,50, 70,90),
-          xlab = "p value", ylab = "genes", main = "A smoking good plot on KOL",
+          xlab = "p value", ylab = "genes", main = "Clustering of gene expression by statistical significance",
           plot_method = "plotly", row_dend_left = TRUE
 ) 
-
 
 # PCA data preparation
 prefit <- ciliated_gois %>% 
